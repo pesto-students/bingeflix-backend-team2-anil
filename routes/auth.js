07555d575732da3ae26f223aed16e6594ab9dcff
Abhijeet Deshmukh <abhijeetd700@gmail.com>
2022-10-22 19:02:06 +0530

    Auth & user api done

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -9,6 +10,7 @@ router.post("/register", async (req, res) => {
       username: req.body.username,
       email: req.body.email,
       password: CryptoJS.AES.encrypt(req.body.password,process.env.SECRET_KEY).toString(),
+      // password: bcrypt.hashSync(req.body.password,10)
     })
 
     try {
@@ -23,16 +25,28 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
     try {
+      console.log('********')
+      console.log(req.body)
       const user = await User.findOne({ email: req.body.email });
 
       if (!user) {
-        res.status(401).json("Wrong password or username!");
+        res.status(401).json("User not found");
         return;
       }
-  
+      console.log('Found',user.password)
       const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+      // console.log(bytes)
       const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
-  
+      
+      console.log('Original Password ',originalPassword)
+
+      console.log('Req password ',req.body.password)
+      console.log('**************')
+
+      // if(!bcrypt.compareSync(user.password,req.body.password)){
+      //   res.status(401).json("Wrong password or username!");
+      //   return;
+      // }
       if(originalPassword !== req.body.password){
         res.status(401).json("Wrong password or username!");
         return;
