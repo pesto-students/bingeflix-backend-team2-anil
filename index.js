07d6a234778eb7671dc58fec7d00e3330e8375b5
Abhijeet Deshmukh <abhijeetd700@gmail.com>
2022-11-02 22:58:53 +0530

    Modified movie models

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies")
+const listRoute = require("./routes/lists")
+const path = require('path');
+
 var cors = require('cors')
 
 dotenv.config();
@@ -23,8 +26,14 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies",movieRoute)
+app.use("/api/lists",listRoute)
 
+// app.use(express.static(path.join(__dirname, "/bingeflix-frontend-team2-anil/build")));
 
-app.listen(8080, () => {
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/bingeflix-frontend-team2-anil/build', 'index.html'));
+// });
+
+app.listen(process.env.PORT || 8080, () => {
     console.log("Backend server is running!");
 });
